b1807d3a29667d0466de36290b55a211bc333e9a
saurabh chopade <saurabhchopade3250@gmail.com>
2020-07-10 20:25:10 +0530

    ADDED : Code for minimum fare if distance and time is less

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/service/InvoiceGenerator.java
@@ -3,8 +3,17 @@ package com.bridgelabz.CabInvoiceGenerator.service;
 public class InvoiceGenerator {
     public static final double MINIMUM_COST_PER_KILOMETER = 10;
     public static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
 
+    /**
+     * Fare Calculator
+     *
+     * @param distance
+     * @param time
+     * @return
+     */
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
